d2163e85d51daeed5cbc180d1557a6fa8f7228f7
donaco <dnc@TRX40>
2025-11-26 21:40:24 +0900

    Revert "艦隊詳細の表示でクラッシュする不具合を解消"
    This reverts commit b63c17e0f3f773f6d2fae34d8c4d87dc1b0f6881.

--- source/Grabacr07.KanColleViewer/Views/FleetWindow.xaml.cs
+++ source/Grabacr07.KanColleViewer/Views/FleetWindow.xaml.cs
@@ -11,31 +11,9 @@ namespace Grabacr07.KanColleViewer.Views
 
 		public FleetWindow()
 		{
-			try
-			{
-				var logDir = Path.GetDirectoryName(UiLog);
-				if (!string.IsNullOrEmpty(logDir) && !Directory.Exists(logDir))
-				{
-					Directory.CreateDirectory(logDir);
-				}
-				File.AppendAllText(UiLog, $"FleetWindow ctor: {DateTime.Now}\n");
-			}
-			catch (Exception ex)
-			{
-				// ログ書き込みが失敗しても UI の初期化は継続させる
-				Debug.WriteLine($"FleetWindow: failed to append ui log in ctor: {ex}");
-			}
-
+			File.AppendAllText(UiLog, $"FleetWindow ctor: {DateTime.Now}\n");
 			InitializeComponent();
-
-			try
-			{
-				File.AppendAllText(UiLog, $"FleetWindow InitializeComponent done: {DateTime.Now}\n");
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine($"FleetWindow: failed to append ui log after InitializeComponent: {ex}");
-			}
+			File.AppendAllText(UiLog, $"FleetWindow InitializeComponent done: {DateTime.Now}\n");
 		}
 	}
 }
