835729a43f0ae45126929e60134bf97ef4d688f6
donaco <dnc@TRX40>
2026-03-04 18:22:46 +0900

    Win10 Pro 環境で艦隊詳細が表示されない不具合を解消

--- source/Grabacr07.KanColleViewer/Views/FleetWindow.xaml.cs
+++ source/Grabacr07.KanColleViewer/Views/FleetWindow.xaml.cs
@@ -7,13 +7,9 @@ namespace Grabacr07.KanColleViewer.Views
 {
 	public partial class FleetWindow
 	{
-		private static readonly string UiLog = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "KanColleViewer", "logs", "ui.log");
-
 		public FleetWindow()
 		{
-			File.AppendAllText(UiLog, $"FleetWindow ctor: {DateTime.Now}\n");
 			InitializeComponent();
-			File.AppendAllText(UiLog, $"FleetWindow InitializeComponent done: {DateTime.Now}\n");
 		}
 	}
 }
